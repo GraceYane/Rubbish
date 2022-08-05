479d8bbdcf49c00505f500e09589a66b3fb95f06
Yan484215571 <484215571@qq.com>
2022-08-05 21:32:32 +0800

    8-5

---
+++ 实战项目/src/main/java/com/example/shizhan/reggie/mapper/RodeMapper.java
@@ -0,0 +1,9 @@
+package com.example.shizhan.reggie.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.shizhan.reggie.entity.Rode;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface RodeMapper extends BaseMapper<Rode> {
+}
---
+++ 实战项目/src/main/java/com/example/shizhan/reggie/service/Interface/RodeService.java
@@ -0,0 +1,9 @@
+package com.example.shizhan.reggie.service.Interface;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.example.shizhan.reggie.entity.Rode;
+import org.springframework.stereotype.Service;
+
+
+public interface RodeService extends IService<Rode> {
+}
