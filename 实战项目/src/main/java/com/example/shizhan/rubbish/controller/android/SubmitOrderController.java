7b60d5b4dd1e80708bc73c1a90ac859674737565
yan <484215571@qq.com>
2022-08-13 09:35:58 +0800

    整合

--- 实战项目/src/main/java/com/example/shizhan/rubbish/controller/android/SubmitOrderController.java
+++ 实战项目/src/main/java/com/example/shizhan/rubbish/controller/android/SubmitOrderController.java
@@ -1,18 +1,17 @@
-package com.example.shizhan.reggie.controller.android;
+package com.example.shizhan.rubbish.controller.android;
 
-import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.example.shizhan.reggie.common.R;
-import com.example.shizhan.reggie.entity.orders;
-import org.apache.commons.lang.StringUtils;
-import com.example.shizhan.reggie.service.Interface.orderService;
+import com.example.shizhan.rubbish.common.R;
+import com.example.shizhan.rubbish.entity.orders;
+import com.example.shizhan.rubbish.service.Interface.orderService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigInteger;
 import java.time.LocalDateTime;
-import java.util.Map;
 
 
 /**
