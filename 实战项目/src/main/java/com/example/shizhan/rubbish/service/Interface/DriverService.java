83dc9ddca2fea0c361943860292b3041691f2a7f
yan <484215571@qq.com>
2022-08-14 12:25:47 +0800

    d

--- 实战项目/src/main/java/com/example/shizhan/rubbish/service/Interface/DriverService.java
+++
@@ -1,7 +0,0 @@
-package com.example.shizhan.rubbish.service.Interface;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.example.shizhan.rubbish.entity.Driver;
-
-public interface DriverService extends IService<Driver> {
-}
