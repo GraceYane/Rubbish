f351860a85f152ee71951474755f90c49c80b9e7
yan <484215571@qq.com>
2022-08-14 19:02:02 +0800

    login

---
+++ 实战项目/src/main/java/com/example/shizhan/rubbish/service/Interface/UserService.java
@@ -0,0 +1,10 @@
+package com.example.shizhan.rubbish.service.Interface;
+
+import com.example.shizhan.rubbish.entity.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * 安卓用户
+ */
+public interface UserService extends IService<User> {
+}
