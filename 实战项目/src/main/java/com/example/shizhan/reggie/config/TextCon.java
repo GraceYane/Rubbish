3507ba770619e89e25463630ad9e62e04aadfb86
TLWToComplex <97268818+TLWToComplex@users.noreply.github.com>
2022-08-15 18:16:08 +0800

    Delete 实战项目 directory

--- 实战项目/src/main/java/com/example/shizhan/reggie/config/TextCon.java
+++
@@ -1,9 +0,0 @@
-package com.example.shizhan.reggie.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("com.example.shizhan.reggie.entity")
-public class TextCon {
-}
